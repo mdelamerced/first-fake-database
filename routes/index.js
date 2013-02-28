2f7872a8d5760c4e63ddb6cd787ba8aea1d383d3
Melissa <melilssadelamerced@gmail.com>
2013-02-28 14:51:51 -0500

    fixes detail.html

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ exports.detail = function(req, res) {
 	}
 
 	var templateData = {
-		article : currentArticle,
+		article_n : currentArticle,
 		article : articles,
 		pageTitle : currentArticle.headline
 	}
@@ -96,14 +96,14 @@ articles.push({
 	slug : 'johnson_wins_2nd_daytona_500_patrick_finishes_8th',
 	headline : 'JOHNSON WINS 2ND DAYTONA 500; PATRICK FINISHES 8TH',
 	timepost : 'Feb 24, 8:11 PM EST',
-	twitter : ['@apnews'],
+	twitter : '@apnews',
 	url : 'http://hosted.ap.org/dynamic/stories/C/CAR_NASCAR_DAYTONA_500?SITE=AP&SECTION=HOME&TEMPLATE=DEFAULT&CTIME=2013-02-24-20-11-36',
 	source : {
 		name : 'Wikipedia',
 		url : 'http://en.wikipedia.org/wiki/John_Glenn'
 	},
 	text : 'Test pilot',
-	//category : false
+	category : true
 });
 
 articles.push({
@@ -111,7 +111,7 @@ articles.push({
 	headline : 'John Glenn',
 	timepost : 'July 18, 1921',
 	twitter : ['Mercury-Atlas 6','STS-95'],
-	photo : 'http://upload.wikimedia.org/wikipedia/commons/thumb/9/93/GPN-2000-001027.jpg/394px-GPN-2000-001027.jpg',
+	utl : 'http://upload.wikimedia.org/wikipedia/commons/thumb/9/93/GPN-2000-001027.jpg/394px-GPN-2000-001027.jpg',
 	source : {
 		name : 'Wikipedia',
 		url : 'http://en.wikipedia.org/wiki/John_Glenn'
